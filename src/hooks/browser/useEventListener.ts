e40692ec8731b8f0e78a379c51ded6b6d0e1e4a3
CuriosBasant <basantbrpl@gmail.com>
2022-04-20 13:46:35 +0530

    remove unnecessary used refs

--- src/hooks/browser/useEventListener.ts
+++ src/hooks/browser/useEventListener.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useEffect, useRef } from "react"
 
 function useEventListener<K extends keyof WindowEventMap>(
@@ -16,15 +17,9 @@ function useEventListener<K extends keyof WindowEventMap>(
   element = typeof window == "undefined" ? null : window,
   options: boolean | AddEventListenerOptions | undefined = undefined
 ) {
-  const callbackRef = useRef(listener)
-
-  useEffect(() => {
-    callbackRef.current = listener
-  }, [listener])
-
   useEffect(() => {
     if (element == null) return
-    const handler = (e: WindowEventMap[K]) => callbackRef.current(e)
+    const handler = (e: WindowEventMap[K]) => listener(e)
     element.addEventListener(eventType, handler, options)
 
     return () => element.removeEventListener(eventType, handler, options)
